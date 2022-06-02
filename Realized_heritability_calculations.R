a6a62029e7b0c8d6c49b7e60da69f71f19c40763
milaleonie <63467079+milaleonie@users.noreply.github.com>
2022-06-02 10:15:53 +0200

    Add files via upload

---
+++ Realized_heritability_calculations.R
@@ -0,0 +1,26 @@
+### Calculate the realized heritability according to the Breeder's equation ----
+rm(list = ls())
+windowsFonts(my = windowsFont('Calibri Light'))
+library(ggplot2)
+library(stringr)
+library(data.table)
+
+setwd("//winfs-uni.top.gwdg.de/mtost$/Masterarbeit/Data_analysis/phenotypic_data/")
+data <- read.table("data_all_years.txt")
+data <-  as.data.table(data)
+
+Short_1_2016 <- data[Population == "Short_plants_1" & year == "2016",]
+Short_1_2017 <- data[Population == "Short_plants_1" & year == "2017",]
+
+calculated_realized_herit <- function(Data_Gen1,
+                                      Data_Gen2){
+  mean_Gen_1 <- mean(Data_Gen1$PlantHeight, na.rm = TRUE)
+  mean_Gen_2 <- mean(Data_Gen2$PlantHeight, na.rm = TRUE)
+  Selected_prop <- quantile(Data_Gen1$PlantHeight, 0.05, na.rm = TRUE)
+  Selection_Differential <- Selected_prop - mean_Gen_1
+  Response_to_Selection <- mean_Gen_2 - mean_Gen_1
+  herit_BS <- as.numeric(Response_to_Selection)/as.numeric(Selection_Differential)
+  return(herit_BS)
+}
+calculated_realized_herit(Data_Gen1 = Short_1_2016,
+                          Data_Gen2 = Short_1_2017)
